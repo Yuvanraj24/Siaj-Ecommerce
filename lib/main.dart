8585836d3116ed1fa1f80345557e7501a7f73c03
Yuvanraj24 <itsmeyuvanraj@gmail.com>
2024-09-25 00:33:29 +0530

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:siaj_ecommerce/app.dart';
+import 'package:siaj_ecommerce/firebase_options.dart';
 
-void main(){
+Future<void> main() async {
+
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
+  // Todo: Await Native Splash
+  // Todo: Initiative Firebase
+
+  await  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then(
+      (FirebaseApp value) => Get.put(AuthenticationRepository())
+  );
+  
   runApp(const MyApp()
   );
 }
